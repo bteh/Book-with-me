ed07710917d7625c70512574cae653ca38b58c1e
Brian Teh <brianteh95@gmail.com>
2018-08-22 19:16:56 -0700

    created rental-card

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './shared/Header';
+import { RentalCard } from './components/rental/RentalCard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <Header />
-       I am a
-      </div>
+       <div className='container'>
+		  <section id='rentalListing'>
+		    <h1 className='page-title'>Your Home All Around the World</h1>
+		    <div className='row'>
+		    <RentalCard />
+		    <RentalCard />
+		    <RentalCard />
+		    </div>
+		  </section>
+		</div>
+    </div>
     );
   }
 }
