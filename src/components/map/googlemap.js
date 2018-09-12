488bf612eec5a9c2316331d8a084975b97b05de6
Brian Teh <brianteh95@gmail.com>
2018-09-11 17:32:40 -0700

    Getting Rental from server and improving rental details, styling, google api, and displays location on map

---
+++ src/components/map/googlemap.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  Circle,
+} from "react-google-maps";
+
+function MapComponent(props){
+  const coordinates = props.coordinates;
+
+  return (
+       <GoogleMap
+      defaultZoom={14}
+      defaultCenter={coordinates}
+      center={coordinates}
+    >
+      <Circle
+        center={coordinates}
+        radius={500}
+      />
+    </GoogleMap>
+    )
+}
+
+
+
+function withGeocode(WrappedComponent){
+  return class extends React.Component {
+    constructor(){
+      super();
+
+      this.state={
+        coordinates: {
+          lat: 0,
+          lng: 0
+        }
+      }
+    }
+    componentWillMount(){
+      this.geocodeLocation();
+
+    }
+
+    geocodeLocation(){
+      const location = this.props.location
+      const geocoder = new window.google.maps.Geocoder(); 
+
+      geocoder.geocode({address: location}, (result, status) => {   
+       if(status === 'OK'){
+         const geometry = result[0].geometry.location;
+         const coordinates = { lat: geometry.lat(), lng: geometry.lng()};
+         this.setState({
+          coordinates: coordinates
+         })
+       }
+      })
+    }
+
+
+    render() {
+      return(
+        <WrappedComponent {...this.state}/>
+        )
+    }
+  }
+}
+
+export const MapWithGeocode = withScriptjs(withGoogleMap(withGeocode(MapComponent)));
+
