f3a1c7bd610efcaec2dcebb02738dfb97d0ee66d
Brian Teh <brianteh95@gmail.com>
2018-09-21 14:50:44 -0700

    corrected port issue

--- server/index.js
+++ server/index.js
@@ -26,18 +26,17 @@ app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
 
-if(process.env.NODE_ENV === 'production'){
-	const appPath = path.join(__dirname, '..', 'build');
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'build');
+  app.use(express.static(appPath));
 
-	app.use(express.static(appPath));
-
-	app.get('*', function(req, res) {
-		res.sendFile(path.resolve(appPath, 'index.html'));
-});
-
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'));
+  });
 }
 
-const PORT = process.envPORT || 3001;
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, function(){
 	console.log('Running')
